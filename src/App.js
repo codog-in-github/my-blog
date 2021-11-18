041b8b54b853cc2aed89e05c1cadaef1e9633fab
zjy <zhaojangyu@gmail.com>
2021-11-18 11:24:41 +0800

    some fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import less from './less/main.less'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            date: new Date(),
+        }
+        setInterval(() => {
+            this.setState({
+                date:new Date()
+            })
+        }, 1000);
+    }
+
+    now(){
+        return this.state.date.valueOf()
+    }
+
+    render(){
+        return <div className="red">{this.now()}</div>
+    }
 }
-
-export default App;
